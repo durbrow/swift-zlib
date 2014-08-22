171c3f01d2a9ba5d1df8b4a6d4dff8a7cab28d36
durbrow <ted@iMac27.local>
2014-08-22 19:25:32 -0400

    Added helper.c

---
+++ swift-zlib/helper.c
@@ -0,0 +1,9 @@
+//
+//  helper.c
+//  swift-zlib
+//
+//  Created by ted on 8/22/14.
+//  Copyright (c) 2014 Kenneth Durbrow. All rights reserved.
+//
+
+#include "helper.h"
---
+++ swift-zlib/helper.h
@@ -0,0 +1,14 @@
+//
+//  helper.h
+//  swift-zlib
+//
+//  Created by ted on 8/22/14.
+//  Copyright (c) 2014 Kenneth Durbrow. All rights reserved.
+//
+
+#ifndef __swift_zlib__helper__
+#define __swift_zlib__helper__
+
+#include <stdio.h>
+
+#endif /* defined(__swift_zlib__helper__) */
