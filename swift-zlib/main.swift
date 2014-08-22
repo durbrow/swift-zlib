d9322efe38b52e429d1446d534ca2c934f688a5f
durbrow <ted@iMac27.local>
2014-08-22 19:19:27 -0400

    Initial commit

---
+++ swift-zlib/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-zlib
+//
+//  Created by ted on 8/22/14.
+//  Copyright (c) 2014 Kenneth Durbrow. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
